4ee624f74d4c994d6db15c8f1379fb7dd1265901
minscars <minhkhalectu@gmail.com>
2023-09-12 16:09:32 +0700

    Add path constant

---
+++ LibraryManagement/LibraryManagement.DTO/Contants/SystemConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.DTO.Contants
+{
+    public class SystemConstant
+    {
+        public const string IMG_BOOKS_FOLDER = "images/Books/";
+    }
+}
